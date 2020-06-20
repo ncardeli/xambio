9870fabc4db61a74539ec667da86c94ed14f2dfe
Nicolás Cardelino <ncardeli@gmail.com>
2020-06-20 19:28:16 -0300

    Increase home buttons width

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,10 +10,15 @@ function Home({ exchangeRate }) {
 		<Panel type="main" title="¿Buscas vender o comprar dólares?">
 			<p className={`${textColor} mx-auto text-2xl`}>US$1 = ${exchangeRate}</p>
 			<nav className="flex justify-center mt-5">
-				<ButtonLink type="sell" mode="normal" className={`mx-2`} to="/sell">
+				<ButtonLink
+					type="sell"
+					mode="normal"
+					className="mx-2 flex-1"
+					to="/sell"
+				>
 					Vender
 				</ButtonLink>
-				<ButtonLink type="buy" mode="normal" className={`mx-2`} to="/buy">
+				<ButtonLink type="buy" mode="normal" className="mx-2 flex-1" to="/buy">
 					Comprar
 				</ButtonLink>
 			</nav>

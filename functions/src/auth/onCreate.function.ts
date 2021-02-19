f46bb3810b108438918e449941f239b3ee1304e9
Nicolas Cardelino <ncardeli@gmail.com>
2021-02-19 19:24:36 -0300

    Fix return value for function

--- functions/src/auth/onCreate.function.ts
+++ functions/src/auth/onCreate.function.ts
@@ -2,7 +2,7 @@ import { auth } from "firebase-functions";
 import * as admin from "firebase-admin";
 
 export default auth.user().onCreate((user) => {
-  admin.database().ref("users").child(user.uid).set({
+  return admin.database().ref("users").child(user.uid).set({
     email: user.email,
     name: user.displayName,
     createdAt: new Date().getTime(),
